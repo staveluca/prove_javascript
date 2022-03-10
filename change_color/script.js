c4746bb1a0b566bce6c0b024870d4e42539ded64
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-10 10:23:17 +0100

    Aggiunta alert e funzione per cambio colori

---
+++ change_color/script.js
@@ -0,0 +1,7 @@
+alert("Benvenuti nella mia prima pagina con componenti javascript");
+
+function changeColor(){
+    document.getElementById("p1").className="red_paragraph";
+    document.getElementById("p2").className="blue_paragraph";
+    document.getElementById("p4").className="green_paragraph";
+}
